c174b62dab69a52c84a21ce110a9c1de7efb450d
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-03-12 21:53:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const form = document.querySelector(".form-group");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const tbody = document.querySelector("#book-list");
+  const row = document.createElement("tr");
+
+  ["title", "author", "isbn"].forEach(function (field) {
+    const cell = document.createElement("td");
+    cell.innerText = form[field].value;
+    row.appendChild(cell);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.innerHTML = `<span class="material-symbols-outlined"> close </span>`;
+
+  deleteButton.addEventListener("click", function () {
+    row.remove();
+  });
+
+  const cell4 = document.createElement("td");
+  cell4.appendChild(deleteButton);
+  row.appendChild(cell4);
+
+  tbody.appendChild(row);
+  form.reset();
+});
